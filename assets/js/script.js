ec236b90809b2c662c5bdbdf88b4cf958c207335
Mary Margaret <mmlawton15@gmail.com>
2021-12-12 19:27:25 -0500

    almost have the hide and show sections figured out

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 var mmApiKey = "2e2ca0507dda47fa6f94fa93790f0ec0";
 var currentWeather;
 var userZipcode = document.getElementById("searchBar").value;
-console.log(userZipcode);
 var zipcodeUserSearchesFor;
 
 
@@ -10,12 +9,31 @@ var zipcodeUserSearchesFor;
 document.querySelector("#btnSearch").addEventListener('click', function() {
     console.log(userZipcode);
     zipcodeUserSearchesFor = userZipcode;
+    
+    //SHOW/HIDE INITIAL SEARCH BAR
+    const initialSearchBar = document.getElementById("initialSearchBar")
+    if (initialSearchBar.style.display !=="none") {
+        initialSearchBar.style.display = "none";
+    } else {
+        initialSearchBar.style.display = "block";
+    }
+
+    //MAKE THE OTHER SECTIONS VISIBLE
+    const gasStationSection = document.getElementById("gasStationResultHeader")
+    if (gasStationSection.style.display ==='none') {
+        gasStationSection.style.display = 'block';
+    } else {
+        gasStationSection.style.display == 'none';
+    }
     getSearchedZipcodeWeather();
 })
 
+
+
+
 //CODE FOR WEATHER ALERT
 var getSearchedZipcodeWeather = function() {
-    var currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${myApiKey}`)
+    var currentWeather = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${zipcodeUserSearchesFor}&units=imperial&appid=${mmApiKey}`)
     console.log(currentWeather);
 }
 
